81767c99f48819345708bb72d2b16e2d8fea7fa6
takimoto <takimoto@lassic.co.jp>
2013-04-19 15:46:59 +0900

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EstimateWds::Application.config.secret_token = '86cfa830cf55b2dd701e32f0001f9c9744ed805b51f86896f15d7b81cffb868e07b4417b6ea45b60491a70a0bb68854d018125c11b240ba45eb85cee3f047d21'
